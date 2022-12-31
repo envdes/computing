3adf85604b9f130f707235d7c885bf0460962d3f
Zhonghua Zheng <zhonghua.zheng@outlook.com>
2022-12-31 09:59:51 +0000

    added requirements

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'MEDAL-computing'
+project = 'MEDAL Computing'
 copyright = '2022, Zhonghua Zheng'
 author = 'Zhonghua Zheng'
 
@@ -31,6 +31,15 @@ release = 'v0.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+'recommonmark',
+'sphinx_markdown_tables',
+'sphinx.ext.githubpages',
+'nbsphinx',
+'IPython.sphinxext.ipython_console_highlighting',
+'sphinx.ext.autodoc', 
+'numpydoc', 
+'sphinx.ext.autosummary',
+'sphinx.ext.mathjax',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,7 +56,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
